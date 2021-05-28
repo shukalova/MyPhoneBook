fdfd6d2fd79391a2b7a475db6c0c1e52805773c2
millet.ua.su@gmail.com <millet.ua.su@gmail.com>
2021-05-28 15:32:07 +0300

    create design of card

---
+++ MyPhonebook/ViewModels/ContactCardViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyPhonebook.ViewModels
+{
+    public class ContactCardViewModel: BaseViewModel
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _middleName;
+        private string _mobilePhone;
+        private string _homePhone;
+        private string _workPhone;
+        private string _homeAddress;
+        private string _workAddress;
+        private string _note;
+
+        public string FirstName
+        {
+            get => _firstName;
+            set 
+            {
+                _firstName = value;
+                OnPropertyChanged(nameof(FirstName));
+            }
+        }
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged(nameof(LastName));
+            }
+        }
+
+        public string MiddleName
+        {
+            get => _middleName;
+            set
+            {
+                _middleName = value;
+                OnPropertyChanged(nameof(MiddleName));
+            }
+        }
+    }
+}
---
+++ MyPhonebook/ViewModels/PhonebookViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyPhonebook.ViewModels
+{
+    public class PhonebookViewModel: BaseViewModel
+    {
+        private ContactCardViewModel _selectedCard;
+        private string _test;
+
+        public string Test { get => _test; set { _test = value; OnPropertyChanged(nameof(Test)); } }
+        public ContactCardViewModel SelectedCard
+        {
+            get => _selectedCard;
+            set
+            {
+                _selectedCard = value;
+                OnPropertyChanged(nameof(SelectedCard));
+            }
+        } 
+    }
+}
