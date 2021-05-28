51af17e959c56608bc9a8f4a547ce4134796f388
shukalova <65778248+shukalova@users.noreply.github.com>
2021-05-28 18:22:06 +0300

    add context db

---
+++ MyPhonebook/Core/ContactCardModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyPhonebook.Core
+{
+    public class ContactCardModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } 
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public decimal MobilePhone { get; set; }
+        public decimal HomePhone { get; set; }
+        public decimal WorkPhone { get; set; }
+        public string HomeAddress { get; set; }
+        public string WorkAddress { get; set; }
+        public string Note { get; set; }
+    }
+}
